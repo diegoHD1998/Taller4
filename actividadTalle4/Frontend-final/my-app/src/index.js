1f9c0d088ccfade9c63480fdbb33b2d9f4b1e56d
Diego Munoz <diego0433hd@gmail.com>
2020-07-21 16:31:09 -0400

    carpeta views con login

--- actividadTalle4/Frontend-final/my-app/src/index.js
+++ actividadTalle4/Frontend-final/my-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SingIn from './views/login'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {<SingIn/>}
   </React.StrictMode>,
   document.getElementById('root')
 );
